445b9f7d3b60a029a3a15bdb887471b56ed61abb
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-15 22:42:07 -0600

    Button import bug finally fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import "./app.css";
-import Button from "./components/Button"
+import { Button } from "./components/Button";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-      <div className="calc_wrapper">
+      <div className="calc-wrapper">
       <div className="row">
       <Button>7</Button>
       <Button>8</Button>
