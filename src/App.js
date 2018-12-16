fab93e5a69fde81a54a9205af84b1dbd871375e5
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-15 21:58:19 -0600

    Uploading changes to the button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import "./app.css";
-
+import Button from "./components/Button"
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-
-       <h1>Hello world</h1>
+      <div className="calc_wrapper">
+      <div className="row">
+      <Button>7</Button>
+      <Button>8</Button>
+      <Button>9</Button>
+      </div>
+      </div>
+    
       </div>
     );
   }
